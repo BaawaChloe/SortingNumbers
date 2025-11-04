ced5dcc50a8b2f48b70e9d8aba68d85e7cc6fd1e
BaawaChloe <aztheslayer@gmial.com>
2025-11-04 10:01:29 +0800

    Add project files.

---
+++ SortingNumbers/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnSort = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(74, 38)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(130, 37)
+        Me.btnRead.TabIndex = 0
+        Me.btnRead.Text = "Read"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(210, 38)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(121, 37)
+        Me.btnWrite.TabIndex = 1
+        Me.btnWrite.Text = "Write"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(71, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter a number:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(159, 12)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(172, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(74, 81)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(257, 95)
+        Me.ListBox1.TabIndex = 4
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(74, 182)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(257, 37)
+        Me.btnSort.TabIndex = 5
+        Me.btnSort.Text = "Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 237)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.btnRead)
+        Me.Name = "Form1"
+        Me.Text = "Number Sorter"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRead As Button
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnSort As Button
+End Class
---
+++ SortingNumbers/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Dim filePath As String = "Sample.txt
+"
+        Using reader As New StreamReader(filePath, True)
+
+
+        End Using
+    End Sub
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        Dim filePath As String = "Sample.txt"
+        Using writer As New StreamWriter(filePath)
+            writer.WriteLine()
+
+
+        End Using
+
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+
+    End Sub
+End Class
